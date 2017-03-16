66e443c74e20aeb61699ae064650f867b18cce1c
severnsc <severnsc@gmail.com>
2017-03-16 11:56:04 -0400

    Add user tokens to login/logout

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,12 +5,18 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by_email(params['email'])
     if user && user.authenticate(params['password'])
+      user.generate_auth_token!
+      user.save
       respond_with user
     else
-      redirect_to 'http://google.com'
+      render json: { errors: "Invalid email or password" }, status: 422
     end
   end
 
   def destroy
+    user = User.find_by_auth_token(params[:id])
+    user.generate_auth_token!
+    user.save
+    head 204
   end
 end
